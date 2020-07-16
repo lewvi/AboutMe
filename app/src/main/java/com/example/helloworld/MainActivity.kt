5fe4f8c6be24bd8dc65ad3f368964116d2470908
lewvi <56486124+lewvi@users.noreply.github.com>
2020-07-17 01:10:46 +0700

    next step

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.helloworld
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
     }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nicname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nicname_text)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+    
+    private fun updateNickname (view: View) {
+        val editText = findViewById<EditText>(R.id.nicname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+    }
+
+}
+
+private fun clickHandlerFunction(viewThatIsClicked: View) {
+
 }
